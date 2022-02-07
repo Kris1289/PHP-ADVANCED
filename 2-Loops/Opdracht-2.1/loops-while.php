7ec3f5c5e020aa5ae8ecf8991b88bd62b6e0c2cb
Kris <krdelver@gmail.com>
2022-02-07 14:34:04 +0100

    2.1

---
+++ 2-Loops/Opdracht-2.1/loops-while.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$leeftijd = 0;
+    while($leeftijd <= 18) {
+        echo "Ik ben $leeftijd jaar, dus ik mag nog niet stemmen <br>";
+  $leeftijd++;
+}
+echo "Ik ben 18 jaar oud dus ik heb stemrecht!"
+
+?>
+
+</body>
+</html>
