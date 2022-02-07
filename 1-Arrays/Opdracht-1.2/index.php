a32b69666b984f264180505f3f93df42a705de1c
Kris <krdelver@gmail.com>
2022-02-07 13:38:33 +0100

    1.1

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$persoon["voornaam"]   = "Kris";
+$persoon["achternaam"] = "Delver";
+$persoon["leeftijd"]   = 17;
+$persoon["klas"]  = "1C";
+
+echo $persoon['voornaam']. " " . $persoon['achternaam']. " is " .  $persoon['leeftijd']. " jaar oud" .  " en ik zit in klas " . $persoon['klas'] ;
+
+?>
+
+</body>
+</html>
