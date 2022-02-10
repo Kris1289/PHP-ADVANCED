ddc8df0c6a6d0bea874d31043e81fdd1ab28daa1
Kris <krdelver@gmail.com>
2022-02-10 12:57:09 +0100

    2.2

--- 2-Loops/Opdracht-2.2/loops-for.php
+++ 2-Loops/Opdracht-2.2/loops-for.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-for ($x = 0; $x <= 18; $x++) {
+for ($x = 0; $x <= 17; $x++) {
     echo "Ik ben $x jaar, dus ik mag nog niet stemmen <br>";
   }
     echo "Ik ben 18 jaar oud dus ik heb stemrecht!"
