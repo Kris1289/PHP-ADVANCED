8eec2567c8df81b458a3f605a26cee9d78b8533c
Kris <krdelver@gmail.com>
2022-02-15 10:05:40 +0100

    3.1

---
+++ 3-Forms/Opdracht-3.1/mijngegevens.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo"voornaam: " .$_POST["name"];
+
+echo"<br>";
+
+echo"achternaam: " .$_POST["achternaam"];
+
+echo"<br>";
+
+echo"klas: " .$_POST["klas"];
+
+echo"<br>";
+
+echo"leeftijd: " .$_POST["leeftijd"];
+
+echo"<br>";
+
+echo"adres: " .$_POST["adres"];
+
+echo"<br>";
+
+echo"plaatsnaam: " .$_POST["plaatsnaam"];
+
+echo"<br>";
+
+echo"favoriete muziekband: " .$_POST["favoriete_muziekband"];
+
+echo"<br>";
+
+
+
+
+?>
+</body>
+</html>
