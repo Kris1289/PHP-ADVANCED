e1716ff963ffeac875940cc04741827a83d44b94
Kris <krdelver@gmail.com>
2022-02-17 13:23:52 +0100

    3.3

---
+++ 3-Forms/Opdracht-3.3/mijngegevens.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+    <li>Artist: <?php echo $_GET['art'] ?></li>
+    <li>Titel: <?php echo $_GET['title'] ?></li>
+    <li>Jaar van uitkomst: <?php echo $_GET['jaar'] ?></li>
+    <li>Muzieksoort: <?php echo $_GET['soort'] ?></li>
+</ul>
+
+
+</body>
+</html>
---
+++ 3-Forms/Opdracht-3.3/mijnmuziek.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<form name="3.3" action="mijngegevens.php" method="get"></form>
+
+<button>
+<a href="mijngegevens.php?
+art=Lil Tjay&
+title=F.N&
+jaar=2019&
+muzieks=Hip Hop">Klik hier voor meer informatie!</a>
+</button>
+
+</body>
+</html>
